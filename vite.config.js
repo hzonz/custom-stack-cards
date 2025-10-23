e8db099e620500eda0fdeb76123adfc76ba7ef75
hhh <147622825+hzonz@users.noreply.github.com>
2025-10-23 14:02:05 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/custom-stack-cards.js",
+      formats: ["es"],
+      fileName: () => "custom-stack-cards.js",
+    },
+    outDir: ".", 
+    emptyOutDir: false,
+    minify: "terser",
+    terserOptions: {
+      compress: { drop_console: true }
+  },
+});
